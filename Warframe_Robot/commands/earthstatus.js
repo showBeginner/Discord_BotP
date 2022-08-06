f68aed7ce8bc73bc0a601ac18aa6ae84f23e1778
show <d0r5i5z1z8l0e@gmail.com>
2022-08-06 21:15:28 +0800

    finish earthstatus command!

--- Warframe_Robot/commands/earthstatus.js
+++ Warframe_Robot/commands/earthstatus.js
@@ -1,11 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+
 const axios = require('axios');
 
 async function getStatus(){
     try{
         const response = await axios.get('https://api.warframestat.us/pc/earthCycle');
         const obj = JSON.parse(JSON.stringify(response.data));
-        return obj.state;
+        return obj;
     }catch(error){
         console.log(error)
     }
@@ -16,6 +18,17 @@ module.exports = {
 		.setDescription('Warframe Earth state'),
 	async execute(interaction) {
         var temp = await getStatus();
-        await interaction.reply(temp);
+        const exampleEmbed = new EmbedBuilder()
+        .setColor(0xFFCCFF)
+        .setTitle('Warframe 地球 時間')
+        .setDescription('取得地球時間與剩餘多久')
+        .addFields(
+            { name: '早上/晚上', value: temp.state },
+            //{ name: '\u200B', value: '\u200B' },
+            { name: '剩餘時間', value: temp.timeLeft , inline: true },
+        )
+        .setTimestamp()
+
+        await interaction.reply({ embeds: [exampleEmbed] });
 	},
 };
